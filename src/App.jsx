f4f7a7acdf1136d4b6c5bf605cf2c41c4c758b09
yegorkravchenko <kraver.kravchenko@gmail.com>
2021-05-01 15:38:14 +0200

    feat: add Posts page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Home } from './pages/Home';
+import { Posts } from './pages/Posts';
 import { Error404 } from './pages/Error404';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path="/posts">
+          <Posts />
+        </Route>
         <Route exact path="/">
           <Home />
         </Route>
--- src/components/NavButton.jsx
+++ src/components/NavButton.jsx
@@ -5,6 +5,7 @@ export function NavButton({ text, to }) {
   return (
     <li className="inline-block first:mr-5">
       <NavLink
+        exact
         activeClassName="bg-blue-500"
         className="px-4 py-1.5 rounded bg-gray-900 text-white"
         to={to}
