29ccab8111f135f3057b5eceb71a0f450f2c0068
yegorkravchenko <kraver.kravchenko@gmail.com>
2021-05-03 21:37:28 +0200

    feat: add Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Post() {
+  return (
+    <div className="container bg-yellow-50 bg-opacity-30 mx-auto max-w-md shadow-xl rounded-xl my-32 py-8 px-12">
+      <h3 className="font-bold text-gray-900 text-3xl mb-5">Title</h3>
+      <p className="text-gray-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, quos
+        fugiat suscipit, perspiciatis maxime obcaecati nemo nesciunt quae
+        maiores in provident eligendi ex, quas facilis doloremque.
+      </p>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import { Header } from '../components/Header';
+import { Post } from '../components/Post';
 
 export function Home() {
   return (
     <>
       <Header pageTitle="Home" />
+      <Post />
     </>
   );
 }
